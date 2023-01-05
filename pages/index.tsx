d983b7ecd7c667ec484d305476f74197c11bff44
unknown <gashiarber423@gmail.com>
2023-01-05 22:03:28 +0100

    3CMT

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           </nav>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Dimitri Marco
+              Arber Gashi
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Developer and designer.
@@ -59,7 +59,7 @@ export default function Home() {
               <FaLinkedin />
               <FaGithub />
             </div>
-            <div className=" mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+            <div className=" mx-auto bg-gradient-to-b from-teal-500 rounded-full w-79 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
               <Image src={arberavatar} alt=""layout="fill" objectFit="cover" />
             </div>
           </div>
